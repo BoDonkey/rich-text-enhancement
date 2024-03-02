a06f7ab5375dab956d33125b9f7802d8b4604bc3
Robert Means <robert@robertmeans.net>
2024-03-02 06:32:54 -0500

    try at refactor

--- modules/rich-text-color/ui/apos/components/ColorButton.vue
+++ modules/rich-text-color/ui/apos/components/ColorButton.vue
@@ -1,9 +1,11 @@
 <template>
-  <input
-    type="color"
-    :value="editor.getAttributes('textStyle').color"
-    @input="editor.chain().focus().setColor($event.target.value).run()"
-  >
+  <input type="color" :value="localColorValue" class="apos-rich-text-editor__control" :label="tool.label"
+    :icon-only="!!tool.icon" :icon="tool.icon || false" :icon-size="tool.iconSize || 16"
+    :modifiers="['no-border', 'no-motion']" :tooltip="{
+    content: tool.label,
+    placement: 'top',
+    delay: 650
+  }" @input="handleColorChange($event.target.value)">
 </template>
 
 <script>
@@ -24,21 +26,35 @@ export default {
       required: true
     }
   },
-  computed: {
-    active() {
-      // The parameters passed to isActive are unpredictable.
-      // If they do not follow the pattern isActive(NAME, OPTIONS)
-      // they should include their own helper
-      if (this.tool.isActive) {
-        return this.editor.isActive(this.tool.isActive);
-      } else {
-        return this.editor.isActive(this.name, this.tool.commandParameters || {});
-      }
+  data() {
+    return {
+      localColorValue: '#000000'
+    };
+  },
+  watch: {
+    // Use a function to watch the editor's textStyle color
+    editor: {
+      handler() {
+        const newColor = this.editor.getAttributes('textStyle').color;
+        if (newColor !== this.localColorValue) {
+          this.localColorValue = newColor || '#000000';
+        }
+      },
+      deep: true, // This is important to watch for nested changes
+      immediate: true // This ensures the watcher is triggered on component mount
+    }
+  },
+  methods: {
+    command() {
+      // This will return the command associated with the tool or the name as a fallback
+      return this.tool.command || this.name;
+    },
+    handleColorChange($event) {
+      console.log('handleColorChange', this.command(), $event);
+      this.editor.chain().focus()[this.command()]($event).run();
     }
   }
 };
 </script>
 
-<style lang="scss" scoped>
-</style>
-
+<style lang="scss" scoped></style>
