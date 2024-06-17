74feb91b551ee219d5414bb57c55f0094b469c37
Robert Means <robert@robertmeans.net>
2024-06-17 09:35:56 -0400

    Still trying refactor

--- modules/rich-text-color/ui/apos/components/ColorButton.vue
+++ modules/rich-text-color/ui/apos/components/ColorButton.vue
@@ -17,32 +17,20 @@
         'apos-has-selection': hasSelection
       }"
     >
-      <div v-if="editor" class="text-color-component">
-        <AposInputWrapper
-          :field="field"
-          :error="effectiveError"
-          :uid="uid"
-          :modifiers="modifiers"
-        >
-          <template #body>
-            <div class="custom-input-object">
-              <div class="custom-input-wrapper">
-                <AposSchema
-                  v-model="colorSchemaInput"
-                  :schema="colorSchema"
-                  :trigger-validation="triggerValidation"
-                  :generation="generation"
-                  @input="handleColorChange"
-                />
-              </div>
-            </div>
-          </template>
-        </AposInputWrapper>
+      <div
+        v-if="editor"
+        class="text-color-component"
+      >
+        <div class="custom-input-object">
+          <div class="custom-input-wrapper">
+            {# Add picker component here? #}
+          </div>
+        </div>
         <footer class="apos-cc-toolbar__footer">
           <AposButton
             type="primary"
             label="apostrophe:close"
-            :modifiers="formModifiers"
+            :modifiers="['small', 'margin-micro']"
             @click="close"
           />
         </footer>
@@ -53,30 +41,16 @@
 
 <script>
 import { ref, watch, defineComponent, computed } from 'vue';
-import AposInputMixin from 'apostrophe/modules/@apostrophecms/schema/ui/apos/mixins/AposInputMixin';
-import AposInputWrapper from 'apostrophe/modules/@apostrophecms/schema/ui/apos/components/AposInputWrapper.vue';
-import AposSchema from 'apostrophe/modules/@apostrophecms/schema/ui/apos/components/AposSchema.vue';
+import AposButton from 'apostrophe/modules/@apostrophecms/ui/ui/apos/components/AposButton.vue';
+import AposInputColor from 'apostrophe/modules/@apostrophecms/schema/ui/apos/components/AposInputColor.vue';
 
 export default defineComponent({
   name: 'ColorButton',
   components: {
-    AposInputWrapper,
-    AposSchema
+    AposButton,
+    AposInputColor
   },
-  mixins: [ AposInputMixin ],
   props: {
-    name: {
-      type: String,
-      required: true
-    },
-    editor: {
-      type: Object,
-      required: true
-    },
-    tool: {
-      type: Object,
-      required: true
-    },
     generation: {
       type: Number,
       required: false,
@@ -84,33 +58,26 @@ export default defineComponent({
         return null;
       }
     },
+    tool: {
+      type: Object,
+      required: true
+    },
     modelValue: {
+      type: Object,
+      default() {
+        return {};
+      }
+    },
+    editor: {
       type: Object,
       required: true
     }
   },
   setup(props) {
     const localColorValue = ref('#000000');
-    const colorSchemaInput = ref({});
     const active = ref(false);
     const buttonActive = ref(false);
     const hasSelection = ref(false);
-    // const field = ref({});
-    // const effectiveError = ref(null);
-    // const uid = ref(null);
-    // const modifiers = ref([]);
-    const colorSchema = [
-      {
-        name: 'color',
-        label: 'Color',
-        type: 'color',
-        required: true,
-        def: '#000000'
-      }
-    ];
-    // const triggerValidation = ref(false);
-    // const generation = ref(0);
-    // const formModifiers = ref([]);
 
     watch(() => props.editor, (newEditor) => {
       if (hasSelection.value) {
@@ -133,7 +100,7 @@ export default defineComponent({
     const takeAction = () => {
       active.value = !active.value;
       if (active.value) {
-        populateFields();
+        console.log('take action');
       }
     };
 
@@ -160,11 +127,9 @@ export default defineComponent({
 
     return {
       localColorValue,
-      colorSchemaInput,
       active,
       buttonActive,
       hasSelection,
-      colorSchema,
       handleColorChange,
       command,
       takeAction,
